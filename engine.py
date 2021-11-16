8a9a5ce1ca6c733e7eb25c01c72988f4a9584f11
VirtuallDev <nitaystain090@gmail.com>
2021-11-16 18:00:37 +0200

    basic window no idea if working

--- engine.py
+++ engine.py
@@ -1,4 +1,15 @@
 import pygame
 
 class Game:
-    pass
+    def __init__(self, title, width, height, start, update):
+        self.screen = pygame.display.set_mode((width, height), 0, 32)
+        self.title = title
+        self.width = width
+        self.height = height
+        self.start = start
+        self.update = update
+
+        self.start()
+        self.update()
+    
+
